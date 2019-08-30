7d8d372eb5ba197e99724dfe1bc3ed0a65b8221b
James Mitchell <jamesmitchelldesign1@gmail.com>
2019-08-30 17:05:16 +1200

    First Task Complete

--- src/arraylisttasks/ArrayListTasks.java
+++ src/arraylisttasks/ArrayListTasks.java
@@ -5,17 +5,47 @@
  */
 package arraylisttasks;
 
+import java.util.ArrayList;
+import static java.util.Collections.list;
+
 /**
  *
  * @author 92020474
  */
 public class ArrayListTasks {
+    
+    static ArrayList<String> shoppingList = new ArrayList<>();//Creating arraylist
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+      shoppingList.add("Juice");//Adding object in arraylist    
+      shoppingList.add("Bread");    
+      shoppingList.add("Butter");    
+      shoppingList.add("Cheese");
+      
+      System.out.println("\n" + "List 1:");
+      printArrayList();
+      
+      shoppingList.add("Eggs");    
+      shoppingList.add("Onions");
+      
+      System.out.println("\n" + "List 2:");
+      printArrayList();
+      
+      shoppingList.remove("Bread");    
+      shoppingList.remove("Butter");
+      
+      System.out.println("\n" + "List 3:");
+      printArrayList();
     }
     
+    static void printArrayList() {
+        //Invoking arraylist object
+        shoppingList.forEach((string) -> {
+            System.out.println(string);
+        });
+    }
+  
 }
