cc8819e5f100451d58149f0606b74ce736336305
James Mitchell <jamesmitchelldesign1@gmail.com>
2019-09-02 13:04:53 +1200

    Second Task Complete

--- src/arraylisttasks/ArrayListTasks.java
+++ src/arraylisttasks/ArrayListTasks.java
@@ -6,46 +6,59 @@
 package arraylisttasks;
 
 import java.util.ArrayList;
-import static java.util.Collections.list;
 
 /**
  *
  * @author 92020474
  */
 public class ArrayListTasks {
-    
-    static ArrayList<String> shoppingList = new ArrayList<>();//Creating arraylist
 
+    public static ArrayList<String> shoppingList = new ArrayList<>();//Creating arraylist
+    public static ArrayList mixedArrayList = new ArrayList();
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      shoppingList.add("Juice");//Adding object in arraylist    
-      shoppingList.add("Bread");    
-      shoppingList.add("Butter");    
+      shoppingList.add("Juice");//Adding object in arraylist
+      shoppingList.add("Bread");
+      shoppingList.add("Butter");
       shoppingList.add("Cheese");
-      
+
       System.out.println("\n" + "List 1:");
       printArrayList();
-      
-      shoppingList.add("Eggs");    
+
+      shoppingList.add("Eggs");
       shoppingList.add("Onions");
-      
+
       System.out.println("\n" + "List 2:");
       printArrayList();
       
-      shoppingList.remove("Bread");    
+      shoppingList.remove("Bread");
       shoppingList.remove("Butter");
-      
+
       System.out.println("\n" + "List 3:");
       printArrayList();
+
+      //Persons Detail List
+      //TASK 2: Adding elements to the mixedArrayList
+        mixedArrayList.add(1);
+        mixedArrayList.add("John Smith");
+        mixedArrayList.add(70.2);
+
+        //TASK 2: Creating new Person object
+        Person person1 = new Person(Integer.valueOf(mixedArrayList.get(0).toString()),
+        mixedArrayList.get(1).toString(), Float.valueOf(mixedArrayList.get(2).toString()));
+
+        //TASK 2: printing our person1's information
+        System.out.println("\n" + person1);
     }
-    
+
     static void printArrayList() {
         //Invoking arraylist object
         shoppingList.forEach((string) -> {
             System.out.println(string);
         });
     }
-  
+
 }
